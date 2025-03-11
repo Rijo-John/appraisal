b92cbb32529264d51553cd1bf462662e6161b4d5
sooraj.r <sooraj.r@thinkpalm.com>
2025-03-11 11:31:04 +0530

    commit with add the success and error message div

--- resources/views/my_appraisal.blade.php
+++ resources/views/my_appraisal.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
           <div class="col">
             @include('layouts.sidebarmenu') 
-            
+
             @php
                 // Retrieve the user's department from the session
                 $userDepartment = session('userDepartment');
@@ -12,6 +12,13 @@
                 // Retrieve non-technical department IDs from .env and convert them to an array
                 $nonTechnicalDepartments = explode(',', env('NON_TECHNICAL_DEPARTMENT_IDS', ''));
             @endphp
+            @if (session('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+
             @if ($errors->any())
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
                     <ul>
